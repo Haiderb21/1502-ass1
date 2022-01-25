78f69e3622ec65ef7531182ad389bdd30385e2aa
Haider <hbang011@mtroyal.ca>
2022-01-24 17:45:29 -0700

    thankyou

--- src/mru/game/view/AppMenu.java
+++ src/mru/game/view/AppMenu.java
@@ -7,4 +7,17 @@ public class AppMenu {
 	 * This class will be used to show the menus and sub menus to the user
 	 * It also prompts the user for the inputs and validates them 
 	 */
+
+
+public static void printMenu() {
+	System.out.println("Select one of these options:\n");
+	System.out.println("");
+	System.out.println("(P) Play Game");
+	System.out.println("(S) Search");
+	System.out.println("(E) Exit \n");
+	System.out.println("");
+	System.out.println("Enter a choice: ");
+	}
 }
+	
+
